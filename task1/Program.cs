5a0b7c14d532b64779a68803c6ac7974d8ab2eb1
Leo_Amv <leonidamvrosov@yandex.ru>
2022-06-29 03:49:02 +0500

    Task 1 completed!

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+        Console.Clear();
+        Console.WriteLine("Введите первое целое число: ");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите второе целое число: ");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+        if (number1 > number2)
+        {
+            Console.WriteLine($"Число {number1} - большее, а число {number2} - меньшее");
+        }
+        if (number1 < number2)
+        {
+            Console.WriteLine($"Число {number2} - большее, а число {number1} - меньшее");
+        }
+        else
+        {
+            Console.WriteLine($"Числа {number1} и {number2} - равны ");
+        }
+
+    }
+}
